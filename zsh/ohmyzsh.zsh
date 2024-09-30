8260c2335d34b2cf212e7937817eebc3ab236d70
vegar <vegar1201@live.no>
2024-09-30 23:12:34 +0200

    Change powerlevel config file loaction to inside .config folder.

--- zsh/ohmyzsh.zsh
+++ zsh/ohmyzsh.zsh
@@ -112,5 +112,6 @@ source $ZSH/oh-my-zsh.sh
 
 
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+# [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
+[[ ! -f $HOME/.config/zsh/p10k.zsh ]] || source $HOME/.config/zsh/p10k.zsh
 
