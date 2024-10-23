0a317c76735ca927cdfe592d8d6786d4529bf7db
vegar <vegar1201@live.no>
2024-10-23 13:13:08 +0200

    Remove pyenv init from env.
    Swaped to have this configuration ignored, as we will not use pyenv on
    every system. We may make a "extra" folder, or configure all optional
    configurations as oh-my-zsh plugins at a later point.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -5,12 +5,4 @@ export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
 # Add to path
 export PATH="$HOME/.local/bin:$PATH"
 
-# FIXME: This fuckes up colors in vim on host, but fixes colors in docker?
-# export TERM=tmux-256color
-
-# Pyenv
-# export PYENV_ROOT="$HOME/.local/var/pyenv"
-# [[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-# eval "$(pyenv init -)"
-# eval "$(pyenv virtualenv-init -)"
 
