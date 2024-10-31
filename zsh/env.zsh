9a76a08c707f66439be151ef69f110f204697a6f
vegar <vegar1201@live.no>
2024-10-31 16:34:11 +0100

    Fix bat manpage, and set GOPATH.

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,6 +1,8 @@
 # Environment variables
 export EDITOR="nvim"
 export MANPAGER="sh -c 'col -bx | batcat -l man -p'"
+export MANROFFOPT='-c'
+export GOPATH="$HOME/.local/share/go"
 
 # Add to path
 export PATH="$HOME/.local/bin:$PATH"
